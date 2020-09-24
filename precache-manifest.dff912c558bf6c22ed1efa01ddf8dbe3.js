c8fc7cc4b6a75fa5b531f5bdf26932f1e9df32d7
SunnyBarua <sunnybarua95@gmail.com>
2020-09-24 15:22:59 +0600

    Updates

---
+++ precache-manifest.dff912c558bf6c22ed1efa01ddf8dbe3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df5319814bf313385f51ccd3126ad6d",
+    "url": "/seo-agency-site-react-styledComponent/index.html"
+  },
+  {
+    "revision": "4dbc404b68803a2bb7da",
+    "url": "/seo-agency-site-react-styledComponent/static/js/2.7d122454.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/seo-agency-site-react-styledComponent/static/js/2.7d122454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b09f836b6189fd7e43ff",
+    "url": "/seo-agency-site-react-styledComponent/static/js/main.5a6d3457.chunk.js"
+  },
+  {
+    "revision": "0ecd180efc0f22dabe47",
+    "url": "/seo-agency-site-react-styledComponent/static/js/runtime-main.5d659296.js"
+  },
+  {
+    "revision": "22b025d414a1e0bde53f4350d294c314",
+    "url": "/seo-agency-site-react-styledComponent/static/media/profile.22b025d4.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/seo-agency-site-react-styledComponent/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/seo-agency-site-react-styledComponent/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/seo-agency-site-react-styledComponent/static/media/svg-3.397c455a.svg"
+  }
+]);
